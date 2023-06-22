5a544828325728f3a00b870e33bcfcba79ca276d
Mikel <mikelavila@hotmail.com>
2023-06-22 18:53:34 -0300

    Avila1-7

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import { connect } from 'mongoose';
 import router from './routes/index.js';
 import error_handler from './middlewares/error_handler.js';
 import not_found_handler from './middlewares/not_found.js';
 import { engine } from 'express-handlebars';
 import { __dirname } from './utils.js';
 import manager from './managers/Product.js';
+import mongoose from 'mongoose';
 
 const server = express();
 
@@ -26,5 +28,9 @@ server.use(express.urlencoded({ extended: true }));
 server.use('/', router);
 server.use(error_handler);
 server.use(not_found_handler);
+
+connect('mongodb+srv://Mikel:maik1234@cluster0.tqkzav1.mongodb.net/ecommerce')
+    .then(() => console.log("database conected"))
+    .catch(err => console.log(err))
 
 export default server;
