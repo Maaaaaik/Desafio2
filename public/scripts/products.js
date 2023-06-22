4a7a41e3446595088fce08cf36a3fd8638055389
Mikel <mikelavila@hotmail.com>
2023-06-22 20:34:22 -0300

    fix

--- public/scripts/products.js
+++ public/scripts/products.js
@@ -1,19 +1,78 @@
-fetch('/api/products/')
+fetch('/api/products?page=1')
     .then(res => res.json())
     .then(res => {
+        const currentPage = res.products.page;
+        const totalPages = res.products.totalPages;
 
-        let templates = res.products.docs.map(each => {
-            let template = `
-        <div class="product-card">
-            <img src="${each.thumbnail}" alt="Producto">
-                <h2 class="product-title">${each.title}</h2>
-                <p class="product-price">$${each.price}</p>
-                <p class="product-description">${each.description}</p>
-                <a href="/product_detail.html?id=${each._id}" class="view-more-button">Ver más</a>
-    </div>
-    `
-            return template
-        }).join('')
-        document.getElementById("home").innerHTML = templates
+        // Renderizar los productos
+        const productTemplate = res.products.docs.map(product => `
+      <div class="product-card">
+        <img src="${product.thumbnail}" alt="Producto">
+        <h2 class="product-title">${product.title}</h2>
+        <p class="product-price">$${product.price}</p>
+        <p class="product-description">${product.description}</p>
+        <a href="/product_detail.html?id=${product._id}" class="view-more-button">Ver más</a>
+      </div>
+    `).join('');
+
+        // Renderizar el botón de navegación hacia atrás
+        const prevButton = `<button onclick="navigateToPage(${currentPage - 1})" ${currentPage === 1 ? 'disabled' : ''}>Anterior</button>`;
+
+        // Renderizar el botón de navegación hacia adelante
+        const nextButton = `<button onclick="navigateToPage(${currentPage + 1})" ${currentPage === totalPages ? 'disabled' : ''}>Siguiente</button>`;
+
+        // Renderizar los productos y los botones de navegación
+        const template = `
+      <div id="products">
+        ${productTemplate}
+      </div>
+      <div id="pagination">
+        ${prevButton}
+        ${nextButton}
+      </div>
+    `;
+
+        document.getElementById('home').innerHTML = template;
     })
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
+
+// Función para navegar a una página específica
+function navigateToPage(pageNumber) {
+    fetch(`/api/products?page=${pageNumber}`)
+        .then(res => res.json())
+        .then(res => {
+            const currentPage = res.products.page;
+            const totalPages = res.products.totalPages;
+
+            // Renderizar los productos
+            const productTemplate = res.products.docs.map(product => `
+        <div class="product-card">
+          <img src="${product.thumbnail}" alt="Producto">
+          <h2 class="product-title">${product.title}</h2>
+          <p class="product-price">$${product.price}</p>
+          <p class="product-description">${product.description}</p>
+          <a href="/product_detail.html?id=${product._id}" class="view-more-button">Ver más</a>
+        </div>
+      `).join('');
+
+            // Renderizar el botón de navegación hacia atrás
+            const prevButton = `<button onclick="navigateToPage(${currentPage - 1})" ${currentPage === 1 ? 'disabled' : ''}>Anterior</button>`;
+
+            // Renderizar el botón de navegación hacia adelante
+            const nextButton = `<button onclick="navigateToPage(${currentPage + 1})" ${currentPage === totalPages ? 'disabled' : ''}>Siguiente</button>`;
+
+            // Renderizar los productos y los botones de navegación
+            const template = `
+        <div id="products">
+          ${productTemplate}
+        </div>
+        <div id="pagination">
+          ${prevButton}
+          ${nextButton}
+        </div>
+      `;
+
+            document.getElementById('home').innerHTML = template;
+        })
+        .catch(err => console.log(err));
+}
